e8cb1304ba4f451572092f370f1c677cef3aa382
polmonroig <polmonroig@yahoo.es>
2020-10-10 12:29:19 +0200

    Add utility functions to graph

--- src/graph/graph.h
+++ src/graph/graph.h
@@ -20,6 +20,7 @@ class Graph{
 
     public:
 
+
     /**
      * @brief calculates the number of connected components of a graph in
      *        a computational time of O(|V| + |E|)
@@ -28,8 +29,33 @@ class Graph{
     unsigned int getTotalConnectedComponents() const;
 
     /**
+     * @brief adds a new vertex to the graph with the smallest ID available,
+     *        the id of a site is represented by its position in the array
+     * */
+    void addSite();
+
+    /**
+     * @brief adds an edge between two vertices, as a precondition, the edge must
+     *        not exist already.
+     * */
+    void addBond(SiteID v, SiteID u);
+
+    /**
+     * @brief returns the number of vertices in the graph
+     * */
+    unsigned int totalSites() const;
+
+    /**
+     * @brief returns the total of bonds in a specific site.
+     * */
+    unsigned int totalBonds(SiteID v) const;
+
+
+
+    /**
      * @brief generates a graph G' that is the result of applying site percolation
-     *        to the given graph
+     *        to the given graph, it has a computational time complexity of O(|V| + |E|)
+     *        and a space complexity of O(|V|)
      * @param g is the graph to percolate
      * @param q is the value of of the probability of percolation
      * @returns returns a percolated graph
@@ -38,7 +64,8 @@ class Graph{
 
     /**
      * @brief generates a graph G' that is the result of applying bond percolation
-     *        to the given graph
+     *        to the given graph, it has a computational time complexity of O(|V| + |E|)
+     *        and a space complexity of O(|V|^2)
      * @param g is the graph to percolate
      * @param q is the value of of the probability of percolation
      * @returns returns a percolated graph
@@ -48,12 +75,13 @@ class Graph{
 
     private:
 
+
     /**
      * @brief Contains the list of nodes with their respective edge connecition
      *        it is the computatational representation of the graph, each vertex
      *        is represented by a numerical ID.
      * */
-    std::vector<std::list<SiteID>> nodeList;
+    std::vector<std::list<SiteID>> nodeList; // ADJACENCY LIST OR MATRIX
 
 };
 
