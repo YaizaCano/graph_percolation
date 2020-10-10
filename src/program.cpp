81dcec2da44c75b4fca63fe29ad59496f59bebf1
polmonroig <polmonroig@yahoo.es>
2020-10-10 18:07:20 +0200

    Bond percolation seams to work, but site no

--- src/program.cpp
+++ src/program.cpp
@@ -10,11 +10,12 @@ int main(){
     g.addSite();
     g.addSite();
     g.addBond(0, 1);
-    g.addBond(1, 2);
     g.addBond(2, 3);
     std::cout << "Graph created!" << std::endl;
-    auto components = g.getTotalConnectedComponents(); 
+    auto components = g.getTotalConnectedComponents();
     std::cout << "Connected Components: " << components << std::endl;
-
+    g = Graph::applySitePercolation(g, 0.8);
+    components = g.getTotalConnectedComponents();
+    std::cout << "Connected Components with bond percolation: " << components << std::endl;
     return 0;
 }
