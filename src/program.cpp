f4c205f5dc6f124e3dd6481d1ce08437a60126bf
polmonroig <polmonroig@yahoo.es>
2020-10-12 18:07:35 +0200

    Add difference metric

--- src/program.cpp
+++ src/program.cpp
@@ -4,26 +4,31 @@
 #include "graph/grid.h"
 #include "graph/geometric.h"
 #include "graph/graph.h"
+
 // el rand generator ha de ser directament proporcional al radi
 // en el cas de Geometric ja que si el espai de posicions es molt gran
-// el radi a de ser mes gran per compensar. 
+// el radi a de ser mes gran per compensar.
+
 int main(){
-    RandGenerator::setSeed(44545);
-    int n = 50;
+    RandGenerator::setSeed(445);
+    int n = 700;
     int dim = 3;
     float radius = 2000;
-    Geometric grid(n, dim, radius);
+    Grid grid(n);
 
     Graph g = grid.createGraph();
 
     std::cout << "Graph created!" << std::endl;
     auto components = g.getTotalConnectedComponents();
     float step = 0.05;
+    long int old_components = 0;
     for(float i = 0; i <= 1; i += step){
         std::cout << "Using q=" << i << std::endl;
         auto g_p = Graph::applySitePercolation(g, i);
         components = g_p.getTotalConnectedComponents();
         std::cout << "Connected Components: " << components << std::endl;
+        std::cout << "Differece: " << components - old_components << std::endl;
+        old_components = components;
     }
     return 0;
 }
