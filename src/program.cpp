deddbe50c8c8d4dc71b4d3c3cbe2d9cbe0703271
polmonroig <polmonroig@yahoo.es>
2020-10-15 10:59:42 +0200

    Add csv file

--- src/program.cpp
+++ src/program.cpp
@@ -4,19 +4,21 @@
 #include "graph/grid.h"
 #include "graph/geometric.h"
 #include "graph/graph.h"
+#include "data/csv_file.h"
 
 // el rand generator ha de ser directament proporcional al radi
 // en el cas de Geometric ja que si el espai de posicions es molt gran
 // el radi a de ser mes gran per compensar.
 
 int main(){
+    // define hyperparameters
     RandGenerator::setSeed(445);
-    int n = 500;
+    int n = 10000;
     int dim = 2;
     float radius = 0.7;
-    Grid generator(n);
-    //Geometric generator(n, dim , radius);
-
+    // generate a graph
+    //Grid generator(n);
+    Geometric generator(n, dim , radius);
     Graph g = generator.createGraph();
 
     std::cout << "Graph created!" << std::endl;
